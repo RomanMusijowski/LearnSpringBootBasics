4ec25d8a90175e5db7d0c7ddeca296f0dfcdac07
roman <romanmusiyowskij248@gmail.com>
2022-01-09 20:32:16 +0100

    Initial commit

---
+++ src/main/java/com/example/learnspringbasics/LearnSpringBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnspringbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBasicsApplication.class, args);
+    }
+
+}
